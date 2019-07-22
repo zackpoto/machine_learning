262aadb24c40b009adee96007880832df344ab3c
zack <ztpotoskie@gmail.com>
2019-07-22 14:23:19 -0400

    Commit #1

---
+++ perceptron_model.py
@@ -0,0 +1,7 @@
+CARDS = (
+    (('Gr', 'Mu', 'Pe', 'Pl', 'Sc', 'Wh')),
+    (('Ca', 'Kn', 'Pi', 'Re', 'Ro', 'Wr')),
+    (('Ba', 'Bi', 'Co', 'Di', 'Ha', 'Ki', 'Li', 'Lo', 'St')),
+)
+
+print("hello")
